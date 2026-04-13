eabff86ce67a505852ffbd091478944ca40827a0
Nicolas Pereira <nicolaspereiraxp1@gmail.com>
2026-04-13 20:46:56 -0300

    Deployment ScaleShop Headless - 2026-04-13T23:46:55.130Z

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 // scale_shop_injected_config
-export const INJECTED_STORE_ID = "ee8e3d42-6076-4dc7-9eee-878afdceba5d"; 
+export const INJECTED_STORE_ID = "30d891bb-2693-41a1-be87-ad05ccd99e28"; 
 export const INJECTED_API_URL = "https://scaleoficial.site";
 export const INJECTED_SUBDOMAIN = "glucomax";
 export const IS_HEADLESS = true;
