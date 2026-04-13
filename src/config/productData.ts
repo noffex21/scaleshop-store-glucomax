87f9bde72c50a7cefb1b54199afe1e4ca714ed0b
Nicolas Pereira <nicolaspereiraxp1@gmail.com>
2026-04-13 20:54:15 -0300

    Deployment ScaleShop Headless - 2026-04-13T23:54:14.422Z

--- src/config/productData.ts
+++ src/config/productData.ts
@@ -20,6 +20,8 @@ export const PRODUCT_DATA = {
   "soldCount": "100+",
   "storeLogo": "",
   "storeName": "Minha Loja",
+  "subdomain": "glucomax",
+  "github_url": "https://github.com/noffex21/scaleshop-store-glucomax",
   "description": {
     "short": "",
     "specs": [],
@@ -32,10 +34,37 @@ export const PRODUCT_DATA = {
   "activeGateway": "nitropag",
   "creatorVideos": [],
   "originalPrice": 199.9,
-  "storeProducts": [],
+  "storeProducts": [
+    {
+      "id": "prod-1776124293538",
+      "kits": {
+        "1": {
+          "name": "Kit Padrão",
+          "image": "",
+          "price": 0,
+          "originalPrice": 0
+        }
+      },
+      "name": "GlucoMax",
+      "brand": "Marca",
+      "price": 0,
+      "reviews": [],
+      "description": {
+        "specs": [],
+        "volume": "",
+        "videoUrl": "",
+        "videoThumbnail": ""
+      },
+      "creatorVideos": [],
+      "originalPrice": 0,
+      "carouselImages": [],
+      "recommendations": []
+    }
+  ],
   "carouselImages": [],
   "recommendations": [],
   "paradiseSecretKey": "[REMOVED_FOR_SECURITY]",
+  "github_repo_full_name": "noffex21/scaleshop-store-glucomax",
   "isPlaceholder": false
 };
 export default PRODUCT_DATA;
